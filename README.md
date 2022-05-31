f3957dabc8801486d561895c819641fb043c04c4
jujumilk3 <jujumilk3@gmail.com>
2022-05-31 15:32:11 +0900

    complete issues 4

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 1. `mix deps`: dependencies 확인
 2. `mix deps.get`: dependencies 다운로드
 3. `mix test`
+4. `mix escript.build`: 바이너리로 빌드
 
 ### githubfetch project
 1. `iex -S mix`: 컴파일하며 iex 실행
---
+++ projects/4/issues/lib/issues.ex
@@ -0,0 +1,21 @@
+defmodule Issues do
+  @moduledoc """
+  Documentation for `Issues`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Issues.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def start(_type, _args) do
+  end
+end
