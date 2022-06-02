83ae9d82305fef0c3b91488be3460e9211947565
jujumilk3 <jujumilk3@gmail.com>
2022-06-02 18:55:19 +0900

    spawn examples

---
+++ spawn/spawn1.exs
@@ -0,0 +1,18 @@
+defmodule Spawn1 do
+  def greet do
+    receive do
+      {sender, msg} ->
+        send sender, { :ok, "Hello, #{msg}" }
+    end
+  end
+end
+
+# 클라이언트
+pid = spawn(Spawn1, :greet, [])
+send pid, {self(), "World!"}
+IO.inspect self()
+
+receive do
+  {:ok, msg} ->
+    IO.puts msg
+end
