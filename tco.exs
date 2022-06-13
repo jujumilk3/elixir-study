454e445cce08e21c73f68cf1b0efcd5ad984b4ae
jujumilk3 <jujumilk3@gmail.com>
2022-06-13 14:03:34 +0900

    tco

---
+++ tco.exs
@@ -0,0 +1,14 @@
+defmodule Recursive do
+  def factorial(0), do: 1
+  def factorial(n), do: n * factorial(n-1)
+end
+
+defmodule TailRecursive do
+  def factorial(n), do: _fact(n, 1)
+  defp _fact(0, acc), do: acc
+  defp _fact(n, acc), do: _fact(n-1, acc*n)
+end
+
+number = 100
+IO.puts Recursive.factorial(number)
+IO.puts TailRecursive.factorial(number)
